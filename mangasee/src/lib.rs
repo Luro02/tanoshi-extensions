903bcd817705f2484a9600ee0f425241fc3d47df
GitHub Action <action@github.com>
2020-07-11 02:03:32 +0000

    auto commit

--- mangasee/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod mangasee;
-use mangasee::Mangasee;
-use tanoshi_lib::extensions::PluginRegistrar;
-
-tanoshi_lib::export_plugin!(register);
-
-extern "C" fn register(registrar: &mut dyn PluginRegistrar, _config: Option<&serde_yaml::Value>) {
-    registrar.register_function("mangasee", Box::new(Mangasee{ /* Fields */ }));
-}
